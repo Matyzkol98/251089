e572444170dcbea2da4664b12e3bbf862acf7aaa
Matyzkol98 <56502598+Matyzkol98@users.noreply.github.com>
2019-10-20 23:01:34 +0200

    Add files via upload

---
+++ Plan_lekcji/plan_lekcji.cpp
@@ -0,0 +1,31 @@
+#include "plan_lekcji.h"
+#include "ui_plan_lekcji.h"
+#include <QMessageBox>
+
+Plan_lekcji::Plan_lekcji(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Plan_lekcji)
+{
+    ui->setupUi(this);
+}
+
+Plan_lekcji::~Plan_lekcji()
+{
+    delete ui;
+}
+
+
+void Plan_lekcji::on_pushButton_clicked()
+{
+   close();
+}
+
+void Plan_lekcji::on_pushButton_1_clicked()
+{
+    QMessageBox::about(this, "O programie", "Wersja 1.0.0\nUtworzył Mateusz Kołodziejski\nNr albumu 251089");
+}
+
+void Plan_lekcji::on_pushButton_2_clicked()
+{
+    dodaj_ucznia.show();
+}
